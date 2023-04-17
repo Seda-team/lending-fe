5ac7dc1837ae2a815592ae53755289171bc86ee8
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2023-04-17 14:14:33 +0700

    update: connect

--- src/components/shared/Connect/index.js
+++ src/components/shared/Connect/index.js
@@ -1,15 +1,36 @@
-import {useContext, useEffect} from 'react'
-import { Button, Box, Typography } from '@mui/material'
+import {useState, useContext, useEffect} from 'react'
+import { Button, Box, Typography, Paper } from '@mui/material'
 import { GlobalContext } from '../../context/GlobalState'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import Web3 from "web3"
 
 const Connect = () => {
   const {updateConnect, connect, updateAddress, address, updateWeb3, web3} = useContext(GlobalContext)
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   useEffect(() => {
     if (window.ethereum) {
       const web3 = new Web3(window.ethereum);
       updateWeb3(web3)
+
+      // Listen update account
+      window.ethereum.on('accountsChanged', (accounts) => {
+        if (accounts.length === 0) {
+          updateAddress(null);
+          updateConnect(false)
+        } else {
+          updateAddress(accounts[0]);
+        }
+      });
     } else {
       console.log('MetaMask is not installed');
     }
@@ -51,6 +72,45 @@ const Connect = () => {
           >
               {address.slice(0, 15)} ...
           </Typography>
+          <Box>
+            <Box sx={{display: "flex", alignItems: "center"}}>
+              {open ? <CloseIcon sx={{
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }}onClick={handleClose}/> : <MenuIcon  sx={{
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }} onClick={handleOpen}/> }
+            </Box>
+            <Box sx={open? {position: "absolute", marginTop: "5px"} : {display: "none"}}>            
+              <Paper elevation={1} sx={{
+                width: "100px",
+                backgroundColor: "#1E90FF", 
+                padding: "5px",
+                "&:hover": {
+                  cursor: "pointer"
+                }}}
+                >
+                <Typography textAlign={"center"} variant="body2" sx={{color: "white", height: "20px", fontSize: "14px"}}>
+                  My Credit Info
+                </Typography>
+              </Paper>  
+              <Paper elevation={1} sx={{
+                marginTop: "5px",
+                width: "100px",
+                backgroundColor: "#1E90FF", 
+                padding: "5px",
+                "&:hover": {
+                  cursor: "pointer"
+                }}}>
+                <Typography textAlign={"center"} variant="body2" sx={{color: "white", height: "20px", fontSize: "14px"}}>
+                  Log Out
+                </Typography>
+              </Paper>  
+            </Box>
+          </Box> 
         </Box>
       : 
       <Button
